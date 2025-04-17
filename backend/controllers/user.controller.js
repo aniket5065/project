28894747913209fd2ab1599cda6a6144710e3c16
aniket5065 <aniketmehatar8888@gmail.com>
2025-04-17 17:00:44 +0530

    project

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,6 +15,12 @@ module.exports.registerUser = async(req, res, next) =>{
 
     const {fullname, email, password} = req.body;
 
+    const isUserAlready = await userModel.findOne({email});
+    if(isUserAlready){
+        return res.status(400).json({message: 'User already exists'});
+    }
+
+
     const hashPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
